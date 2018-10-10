3fe3702dffe890a350db21330ffaa3eaba6ac80a
CBreezyNCSU <43019188+CBreezyNCSU@users.noreply.github.com>
2018-10-10 18:10:12 -0400

    Add files via upload

---
+++ lib/IntegerSetArray.cpp
@@ -0,0 +1,45 @@
+/*
+ * IntegerSetArray.cpp
+ *
+ *  Created on: Oct 10, 2018
+ *      Author: Christophe's Book
+ */
+
+#include "IntegerSetArray.h"
+
+#define EMPTY -1
+
+
+IntegerSetArray::IntegerSetArray(int htsize):IntegerSet(htsize) {
+	array = new int[size];
+	int iterator=0;
+	while (iterator < size)
+		array[iterator++] = EMPTY;
+}
+
+bool IntegerSetArray::insert (int data) {
+	for (int iterator=0; iterator < size; iterator++)
+		if (array[iterator] == EMPTY)
+		{
+			array[iterator] = data;
+			return true;
+		}
+	return false;
+}
+
+bool IntegerSetArray::search (int data) const {
+	for (int iterator=0; iterator < size; iterator++)
+		if (array[iterator] == data)
+		{
+			return true;
+		}
+	return false;
+}
+
+void IntegerSetArray::remove (int data) {
+	for (int iterator=0; iterator < size; iterator++)
+		if (array[iterator] == data)
+			array[iterator] = EMPTY;
+}
+
+
