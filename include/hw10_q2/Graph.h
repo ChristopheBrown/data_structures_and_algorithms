ac3369faa794ab2fc9f212c7a3588917790b628f
CBreezyNCSU <43019188+CBreezyNCSU@users.noreply.github.com>
2018-12-02 18:26:44 -0500

    namespace ece309 {
    namespace ece309 {

--- include/hw10_q2/Graph.h
+++ include/hw10_q2/Graph.h
@@ -3,80 +3,84 @@
 
 #include "List.h"
 
-class Graph {
-protected:
-  int numNodes;
+namespace ece309 {
 
-public:
-  Graph(int n) : numNodes(n){};
-  virtual void addEdge(int, int) = 0;
-  virtual bool isAdjacent(int, int) const = 0;
+  class Graph {
+  protected:
+    int numNodes;
 
-  virtual List getAdjacencyList(int) const = 0;
-  virtual int getWeight(int, int) const { return 1; }
-  int getNumNodes() const { return numNodes; }
-};
-
-class SparseGraph : public Graph {
-protected:
-  class Node {
   public:
-    List edge;
+    Graph(int n) : numNodes(n){};
+    virtual void addEdge(int, int) = 0;
+    virtual bool isAdjacent(int, int) const = 0;
+
+    virtual List getAdjacencyList(int) const = 0;
+    virtual int getWeight(int, int) const { return 1; }
+    int getNumNodes() const { return numNodes; }
   };
-  Node *nodes;
 
-public:
-  SparseGraph(int n);
-  void addEdge(int, int) override;
-  bool isAdjacent(int, int) const override;
+  class SparseGraph : public Graph {
+  protected:
+    class Node {
+    public:
+      List edge;
+    };
+    Node *nodes;
 
-  List getAdjacencyList(int node) const override { return nodes[node].edge; }
-};
+  public:
+    SparseGraph(int n);
+    void addEdge(int, int) override;
+    bool isAdjacent(int, int) const override;
 
-class DirectedSparseGraph : public SparseGraph {
-public:
-  DirectedSparseGraph(int n);
-  void addEdge(int, int) override;
-};
+    List getAdjacencyList(int node) const override { return nodes[node].edge; }
+  };
 
-class DenseGraph : public Graph {
-  bool *edges;
+  class DirectedSparseGraph : public SparseGraph {
+  public:
+    DirectedSparseGraph(int n);
+    void addEdge(int, int) override;
+  };
 
-public:
-  DenseGraph(int n);
-  void addEdge(int, int) override;
-  bool isAdjacent(int, int) const override;
+  class DenseGraph : public Graph {
+    bool *edges;
 
-  List getAdjacencyList(int node) const override {
-    List ret;
-    for (int i = 0; i < numNodes; i++) {
-      if (isAdjacent(node, i)) {
-        ret.append(i);
+  public:
+    DenseGraph(int n);
+    void addEdge(int, int) override;
+    bool isAdjacent(int, int) const override;
+
+    List getAdjacencyList(int node) const override {
+      List ret;
+      for (int i = 0; i < numNodes; i++) {
+        if (isAdjacent(node, i)) {
+          ret.append(i);
+        }
       }
+      return ret;
     }
-    return ret;
-  }
-};
+  };
 
-class WeightedDenseGraph : public DenseGraph {
-protected:
-  int *weights;
-  void addWeight(int from, int to, int weight);
+  class WeightedDenseGraph : public DenseGraph {
+  protected:
+    int *weights;
+    void addWeight(int from, int to, int weight);
 
-private:
-  void addEdge(int f, int t) override { DenseGraph::addEdge(f, t); }
+  private:
+    void addEdge(int f, int t) override { DenseGraph::addEdge(f, t); }
 
-public:
-  WeightedDenseGraph(int n) : DenseGraph(n) {
-    weights = new int[n * n];
-    for (int i = 0; i < n * n; i++)
-      weights[i] = 0;
-  }
+  public:
+    WeightedDenseGraph(int n) : DenseGraph(n) {
+      weights = new int[n * n];
+      for (int i = 0; i < n * n; i++)
+        weights[i] = 0;
+    }
 
-  void addEdge(int src, int dest, int weight);
-  int getWeight(int src, int dest) const override {
-    return weights[src * numNodes + dest];
-  }
-};
+    void addEdge(int src, int dest, int weight);
+    int getWeight(int src, int dest) const override {
+      return weights[src * numNodes + dest];
+    }
+  };
 
+  
+}
 #endif // GRAPH_H
