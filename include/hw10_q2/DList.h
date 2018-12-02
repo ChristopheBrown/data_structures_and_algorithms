6ac3ca29551fe4565afa29e67fc8ee3bebaab168
CBreezyNCSU <43019188+CBreezyNCSU@users.noreply.github.com>
2018-12-02 18:25:48 -0500

    namespace ece309 {
    namespace ece309 {

--- include/hw10_q2/DList.h
+++ include/hw10_q2/DList.h
@@ -4,49 +4,50 @@
 #include <stdlib.h>
 
 using Item = int;
-
-class DList {
-private:
-  // DListNode represents each
-  // node of the list
-  class DListNode {
-  public:
-    Item item;
-    DListNode *next;
-    DListNode *prev;
-    DListNode(Item i, DListNode *n = NULL, DListNode *p = NULL) {
-      item = i;
-      next = n;
-      prev = p; // constructs the node for us
-    }
-  };
-
-  // add head and tail pointer
-  DListNode *head;
-  DListNode *tail;
-
-public:
-  class iterator {
+namespace ece309 {
+   
+  class DList {
   private:
-    DListNode *node;
+    // DListNode represents each
+    // node of the list
+    class DListNode {
+    public:
+      Item item;
+      DListNode *next;
+      DListNode *prev;
+      DListNode(Item i, DListNode *n = NULL, DListNode *p = NULL) {
+        item = i;
+        next = n;
+        prev = p; // constructs the node for us
+      }
+    };
+
+    // add head and tail pointer
+    DListNode *head;
+    DListNode *tail;
 
   public:
-    iterator(DListNode *n = NULL) { node = n; }
-    Item &getItem() { return node->item; }
-    void increment() { node = node->next; }
-    bool end() { return node == NULL; }
+    class iterator {
+    private:
+      DListNode *node;
 
-    friend class DList;
+    public:
+      iterator(DListNode *n = NULL) { node = n; }
+      Item &getItem() { return node->item; }
+      void increment() { node = node->next; }
+      bool end() { return node == NULL; }
+
+      friend class DList;
+    };
+
+  public:
+    DList();
+    void append(Item a);
+    void insertAfter(iterator it, Item a);
+    void erase(iterator it);
+    bool empty() { return head == NULL; }
+
+    iterator begin() { return iterator(head); }
   };
-
-public:
-  DList();
-  void append(Item a);
-  void insertAfter(iterator it, Item a);
-  void erase(iterator it);
-  bool empty() { return head == NULL; }
-
-  iterator begin() { return iterator(head); }
-};
-
+}
 #endif // DLIST_H
