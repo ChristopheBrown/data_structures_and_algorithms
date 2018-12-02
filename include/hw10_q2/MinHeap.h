a570885b6a41f38f9e3ba54de6432b5b4437b4c0
CBreezyNCSU <43019188+CBreezyNCSU@users.noreply.github.com>
2018-12-02 18:27:57 -0500

    namespace ece309 {
    namespace ece309 {

--- include/hw10_q2/MinHeap.h
+++ include/hw10_q2/MinHeap.h
@@ -12,39 +12,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-class MinHeap {
-private:
-  int *heapArray;    // this is the maxheap
-  int heapArraySize; // size of the array
-  int nextIndex;     // location of next free array entry
 
-protected:
-  void percolate_up(int);
-  void percolate_down(int, int);
+namespace ece309 {
+  
+  class MinHeap {
+  private:
+    int *heapArray;    // this is the maxheap
+    int heapArraySize; // size of the array
+    int nextIndex;     // location of next free array entry
 
-public:
-  MinHeap(int size = 100) : heapArraySize(size) {
-    heapArray = new int[size];
-    for (int i = 0; i < size; i++)
-      heapArray[i] = 0;
-    nextIndex = 0;
-  }
+  protected:
+    void percolate_up(int);
+    void percolate_down(int, int);
 
-  void print() {
-    for (int i = 0; i < nextIndex; i++) {
-      printf("%d ", heapArray[i]);
+  public:
+    MinHeap(int size = 100) : heapArraySize(size) {
+      heapArray = new int[size];
+      for (int i = 0; i < size; i++)
+        heapArray[i] = 0;
+      nextIndex = 0;
     }
-    printf("\n");
-  }
 
-  int* operator[](int);
-  int getHeapArraySize();
+    void print() {
+      for (int i = 0; i < nextIndex; i++) {
+        printf("%d ", heapArray[i]);
+      }
+      printf("\n");
+    }
 
-  bool insert(int);
-  bool remove(int &); // remove the max value
-  bool isEmpty();
-};
+    int* operator[](int);
+    int getHeapArraySize();
 
+    bool insert(int);
+    bool remove(int &); // remove the max value
+    bool isEmpty();
+  };
 
+}
 
 #endif /* MINHEAP_H_ */
