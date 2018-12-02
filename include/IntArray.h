e9192866b7dbb197e848cd3063f4d0d83d2952d3
CBreezyNCSU <43019188+CBreezyNCSU@users.noreply.github.com>
2018-12-02 18:21:00 -0500

    namespace ece309 {
    namespace ece309 {

--- include/IntArray.h
+++ include/IntArray.h
@@ -11,62 +11,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-class IntArray {
-private:
-	int * array;
-	int length;
-public:
-	IntArray(const char* str);
-	IntArray(int size, int val);
-	int& operator[](int index);
-	IntArray& operator+=(const IntArray &rhs);
-	IntArray operator+(const IntArray &rhs) const;
-	IntArray operator-(const IntArray &rhs) const;
-	IntArray operator*(int x) const;
-	IntArray operator/(int x) const;
-	IntArray operator<<(int count) const;
-	operator char*() const;
-	operator int() const;
+namespace ece309 {
+	class IntArray {
+	private:
+		int * array;
+		int length;
+	public:
+		IntArray(const char* str);
+		IntArray(int size, int val);
+		int& operator[](int index);
+		IntArray& operator+=(const IntArray &rhs);
+		IntArray operator+(const IntArray &rhs) const;
+		IntArray operator-(const IntArray &rhs) const;
+		IntArray operator*(int x) const;
+		IntArray operator/(int x) const;
+		IntArray operator<<(int count) const;
+		operator char*() const;
+		operator int() const;
 
-   ~IntArray() {
-	 delete [] array;
-   }
+	   ~IntArray() {
+		 delete [] array;
+	   }
 
-   IntArray(const IntArray &copy) {
-	 length = copy.length;
-	 array = new int[length];
-	 for(int i=0; i<length; i++)
-	   array[i] = copy.array[i];
-   }
+	   IntArray(const IntArray &copy) {
+		 length = copy.length;
+		 array = new int[length];
+		 for(int i=0; i<length; i++)
+		   array[i] = copy.array[i];
+	   }
 
-   IntArray& operator=(const IntArray &rhs) {
-	 if(this == &rhs) return *this;
-	 printf("Running operator=!\n");
-	 delete [] array;
-	 length = rhs.length;
-	array = new int[length];
-	for(int i=0; i<length; i++)
-	   array[i] = rhs.array[i];
-	return *this; // reference to this object
-   }
+	   IntArray& operator=(const IntArray &rhs) {
+		 if(this == &rhs) return *this;
+		 printf("Running operator=!\n");
+		 delete [] array;
+		 length = rhs.length;
+		array = new int[length];
+		for(int i=0; i<length; i++)
+		   array[i] = rhs.array[i];
+		return *this; // reference to this object
+	   }
 
-  void rand(int M=100) {
-	for(int i=0; i<length; i++)
-	  array[i] = ::rand() % M;
-  }
+	  void rand(int M=100) {
+		for(int i=0; i<length; i++)
+		  array[i] = ::rand() % M;
+	  }
 
-   void set(int index, int val) {
-	 array[index] = val;
-   }
+	   void set(int index, int val) {
+		 array[index] = val;
+	   }
 
-   int get(int index) {
-	   return array[index];
-   }
+	   int get(int index) {
+		   return array[index];
+	   }
 
-	int getLength() { return length; }
-};
+		int getLength() { return length; }
+	};
 
-
+}
 
 #endif /* INTARRAY_H_ */
 
